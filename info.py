5e67659ce384d6d4780673e865412c1325e7f5c0
Amit Mishra <152164391+JoneySin@users.noreply.github.com>
2023-12-05 13:06:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ PORT = int(environ.get('PORT', '8080'))
 
 # Bot pics
 PICS = (environ.get('PICS', 'https://telegra.ph/file/58fef5cb458d5b29b0186.jpg https://telegra.ph/file/f0aa4f433132769f8970c.jpg https://telegra.ph/file/f515fbc2084592eca60a5.jpg https://telegra.ph/file/20dbdcffaa89bd3d09a74.jpg https://telegra.ph/file/6045ba953af4def846238.jpg')).split()
-RemoveBG_API = environ.get("RemoveBG_API", "GdjikGKmhHqBoptnnQsCFGAm")
+
 # Bot Admins
 ADMINS = environ.get('ADMINS', '5069272727')
 if len(ADMINS) == 0:
